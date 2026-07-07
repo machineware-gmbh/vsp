e1b5dfa7981397e2ad28a949477c90089e97fb1f
Jan Henrik Weinstock <jan@mwa.re>
2026-07-07 07:06:24 +0200

    chore: fix typo in copyright header and added missing ones

--- simplevp/simple_vp.cpp
+++ simplevp/simple_vp.cpp
@@ -1,3 +1,13 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2025 MachineWare GmbH                                        *
+ * All Rights Reserved                                                        *
+ *                                                                            *
+ * This work is licensed under the terms described in the LICENSE file found  *
+ * in the root directory of this source tree.                                 *
+ *                                                                            *
+ ******************************************************************************/
+
 #include "simple_cpu.h"
 #include "vcml.h"
 
